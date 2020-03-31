49b051fd18810f40b3cc6a0775f8e1c7db5e69c4
tomasggck <tomasggck@gmail.com>
2020-03-31 14:28:14 -0700

    added dice vector to main

--- main.cc
+++ main.cc
@@ -1,16 +1,33 @@
 //Write your last name and your partner's name here
+//Garcia , Zamudio
 #include <iostream>
+#include<vector>
 #include "die.h"
 using namespace std;
+
 
 int main() {
 	srand(time(0));
 	cout << "Hello World!\n";
-	cout<<"one more gain";
-	Die d({.5,.1,.1,.1,.1,.1},6);
+	vector<Die> playingDice;
+	Die d1;
 	Die d2;
-	d.roll();
-	d2.roll();
-	cout << d.get_roll() << endl;
-	cout << d2.get_roll() << endl;
+	Die d3;
+	Die d4;
+	Die d5;
+	Die d6;
+	playingDice.push_back(d1);
+	playingDice.push_back(d2);
+	playingDice.push_back(d3);
+	playingDice.push_back(d4);
+	playingDice.push_back(d5);
+	playingDice.push_back(d6);
+
+	
+	
+	for(int i=0; i< playingDice.size();i++){
+		playingDice.at(i).roll();
+		cout<<"Die: "<< i <<" "<<playingDice.at(i).get_roll()<<endl;
+		}
+
 }
